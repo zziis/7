f2d48205379943a1304cf1b34826446a2a6e76ab
zilzal <zlzalh810z@gmail.com>
2026-08-31 03:43:24 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.ZILZAL_SUPABASE_URL = "https://ravtwfqrqjycyzqqsutq.supabase.co";
-window.ZILZAL_SUPABASE_ANON_KEY = "sb_secret_F5WIA2theU36_j_xoUEeDg_tVMLLv9c";
+window.ZILZAL_SUPABASE_ANON_KEY = "sb_publishable_1oZf_PAjdZVwWuxsDgcLqQ_7tFZTgBZ";
